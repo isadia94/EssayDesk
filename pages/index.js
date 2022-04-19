63aa2755d9bb0d40f4f7623a6b26ba00ca412b79
isadia94 <brianisadia94@gmail.com>
2022-04-19 04:14:33 +0300

    Finished homepage on large screens

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { MenuIcon } from "@heroicons/react/solid";
+import {
+  MenuIcon,
+  UserAddIcon,
+  QuestionMarkCircleIcon,
+  UserGroupIcon,
+} from "@heroicons/react/solid";
 import { subjects } from "../data/subjects";
 
 export default function Home() {
@@ -15,54 +20,82 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100&display=swap"
           rel="stylesheet"
         ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Noto+Serif+Display:wght@600;800&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
-      <nav className="p-6 flex items-center justify-between">
+      <nav className="p-6 lg:p-4 flex items-center justify-between">
         <logo>
           <Link href="/">
-            <a className="text-3xl font-bold">EssayDesk</a>
+            <a className="text-3xl font-bold logo text-red-500">EssayDesk</a>
           </Link>
         </logo>
-        <MenuIcon className="h-6" />
+        <div className="hidden lg:flex items-center space-x-12 text-[15px] font-[500]">
+          <Link href="/">
+            <a className="hover:text-red-500">Home</a>
+          </Link>
+          <Link href="/">
+            <a className=" hover:text-red-500">Documents</a>
+          </Link>
+          <Link href="/">
+            <a className="hover:text-red-500">Join Our Tutors</a>
+          </Link>
+          <Link href="/">
+            <a className=" hover:text-red-500">Login</a>
+          </Link>
+          <Link href="/">
+            <a className="bg-blue-400 px-5 py-3 rounded-full">Register</a>
+          </Link>
+        </div>
+        <MenuIcon className="h-6 lg:hidden" />
       </nav>
 
       <main className="mt-2">
         <div>
           <div className="relative">
-            <Image
-              src="/hero-image.jpg"
-              alt="hero"
-              width={1200}
-              height={1600}
-              objectFit="cover"
-            />
+            <div className=" h-[500px]">
+              <Image
+                src="/hero-image.jpg"
+                alt="hero"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+
             <div className="absolute top-0 left-0 w-full h-full bg-red-400 opacity-50"></div>
             <div className="absolute top-[150px] pl-6 z-10 flex flex-col space ">
-              <h1 className=" text-3xl font-bold text-white  max-w-[300px]">
+              <h1 className=" text-3xl lg:text-[45px] lg:leading-[3.7rem] font-bold text-white  max-w-[300px] lg:max-w-[500px] ">
                 Get Your Essay done by top professionals
               </h1>
-              <Link className=" " href="/">
-                <a className="mt-4 text-xl font-bold bg-blue-500 py-4 px-2 rounded-full flex items-center justify-center max-w-[200px]">
+              <Link href="/">
+                <a className="mt-4 lg:mt-8 text-xl font-bold bg-blue-500 py-4 px-2 rounded-full flex items-center justify-center max-w-[200px] text-white">
                   Post a Question
                 </a>
               </Link>
-              <p className="text-xl text-white mt-4 ml-20">Or</p>
+              <p className="text-xl text-white mt-4 ml-16">Or</p>
               <Link href="/">
-                <a className="text-xl ml-5 mt-4">Order Answers</a>
+                <a className="text-xl ml-2 mt-3">Obtain Study Documents</a>
               </Link>
             </div>
           </div>
-          <div className="mt-12 text-center ">
+          <div className="mt-12 lg:mb-24 text-center ">
             <h2 className="font-bold text-gray-500 text-3xl">How it Works</h2>
-            <p className="mt-4 bg-gray-300 shadow-2xl mx-6 py-4">
-              Register an Account
-            </p>
-            <p className="mt-4 bg-gray-300 shadow-2xl mx-6 py-4">
-              Post any question you need help with.
-            </p>
-            <p className="mt-4 bg-gray-300 shadow-2xl mx-6 py-2">
-              Get high quality answers from our top tutors
-            </p>
+            <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-y-4 lg:space-y-0">
+              <div className="flex flex-col items-center max-w-[300px] justify-center mx-auto">
+                <UserAddIcon className="h-12 text-red-500" />
+                <p className="">Register an Account</p>
+              </div>
+              <div className="flex flex-col items-center max-w-[300px] justify-center mx-auto">
+                <QuestionMarkCircleIcon className="h-12 text-red-500" />
+                <p className="">Post any question you need help with.</p>
+              </div>
+              <div className="flex flex-col items-center max-w-[300px] justify-center mx-auto">
+                <UserGroupIcon className="h-12 text-red-500" />
+                <p className="">Get high quality answers from our top tutors</p>
+              </div>
+            </div>
           </div>
 
           <div className="mt-12 text-center ">
@@ -71,7 +104,7 @@ export default function Home() {
             </h1>
           </div>
           <div className="">
-            <div className="grid grid-cols-2 mt-8 pl-6 mb-6">
+            <div className="grid grid-cols-2 lg:grid-cols-4 mt-8 pl-6 mb-6">
               {subjects.map((subject) => (
                 <div className="mb-1">
                   <p>{subject.name}</p>
@@ -80,8 +113,8 @@ export default function Home() {
             </div>
             <div className="h-[0.6px] w-full bg-red-400" />
           </div>
-          <div className="h-[300px] w-full py-12 px-6  bg-gray-500 text-white text-center">
-            <div className="flex flex-col space-y-4">
+          <div className="h-[200px] py-12 bg-gray-500 text-white text-center mx-auto">
+            <div className="flex flex-col lg:flex-row max-w-[700px] mx-auto space-y-3 lg:space-y-0">
               <div>
                 <Link href="/">
                   <a>How it Works |</a>
